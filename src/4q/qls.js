2d824ab5b63e5bccd10fb35996422280c9e7d785
Robey Pointer <robeypointer@gmail.com>
2015-04-13 09:41:53 -0700

    qls and qpack.

---
+++ src/4q/qls.js
@@ -0,0 +1,218 @@
+const clicolor = require("clicolor");
+const crypto = require("crypto");
+const fs = require("fs");
+const helpers = require("./helpers");
+const lib4q = require("lib4q");
+const minimist = require("minimist");
+const Promise = require("bluebird");
+const toolkit = require("stream-toolkit");
+const util = require("util");
+
+require("source-map-support").install();
+
+const PACKAGE = require("../../package.json");
+const COLORS = helpers.COLORS;
+
+const USAGE = `
+usage: qls [options] <filename(s)...>
+    displays contents of 4Q archives
+
+options:
+    --help
+    -l
+        long form: display date/time, user/group, and posix permissions
+    -q
+        quiet: display only the summary line at the end
+    --structure
+        show the bottle structure of the archive, instead of the listing
+    --no-color
+        turn off cool console colors
+`;
+
+let password = null;
+
+function main() {
+  const cli = clicolor.cli();
+  const keybaser = new (require("./keybaser").Keybaser)(cli);
+
+  const argv = minimist(process.argv.slice(2), {
+    boolean: [ "help", "version", "l", "q", "color", "structure", "debug" ],
+    default: { color: true }
+  });
+  if (argv.help || argv._.length == 0) {
+    console.log(USAGE);
+    process.exit(0);
+  }
+  if (argv.version) {
+    console.log(`qls ${PACKAGE.version}`);
+    process.exit(0);
+  }
+  if (argv._.length == 0) {
+    console.log("required: filename of 4Q archive file(s)");
+    process.exit(1);
+  }
+  if (!argv.color) cli.useColor(false);
+  cli.quiet(argv.q);
+  if (argv.password) password = argv.password;
+
+  const loudness = { isVerbose: argv.l, isQuiet: argv.q, cli, keybaser };
+  (argv.structure ? dumpArchiveStructures(argv._, loudness) : dumpArchiveFiles(argv._, loudness)).catch((error) => {
+    cli.displayError(`Unable to read archive: ${helpers.messageForError(error)}`);
+    if (argv.debug) console.log(error.stack);
+    process.exit(1);
+  });
+}
+
+function dumpArchiveStructures(filenames, loudness) {
+  return Promise.map(filenames, ((filename) => dumpArchiveStructure(filename, loudness)), { concurrency: 1 });
+}
+
+function dumpArchiveStructure(filename, { isVerbose, isQuiet, cli, keybaser }) {
+  let indent = 0;
+  const pad = () => {
+    let rv = "";
+    while (rv.length < indent) rv += " ";
+    return rv;
+  };
+
+  const reader = new lib4q.ArchiveReader();
+
+  reader.decryptKey = (keymap) => {
+    if (Object.keys(keymap).length == 0) {
+      if (password == null) throw new Error("No password provided.");
+      return Promise.promisify(crypto.pbkdf2)(password, helpers.SALT, 10000, 48);
+    }
+    return keybaser.check().then(() => {
+      const self = `keybase:${keybaser.identity}`;
+      const allowed = Object.keys(keymap).join(", ");
+      if (!keymap[self]) throw new Error(`No encryption key for ${self} (only: ${allowed})`);
+      return keybaser.decrypt(keymap[self]);
+    });
+  };
+
+  reader.on("start-bottle", (bottle) => {
+    const typeName = cli.color("purple", bottle.typeName());
+    let extra = "";
+    switch (bottle.typeName()) {
+      case "file":
+        extra = `${bottle.header.filename} (${bottle.header.size})`;
+        break;
+      case "folder":
+        extra = bottle.header.filename;
+        break;
+    }
+    cli.display(cli.paint(pad(), "+ ", typeName, " ", extra));
+    indent += 2;
+  });
+
+  reader.on("end-bottle", (bottle) => {
+    indent -= 2;
+  });
+
+  reader.on("hash", (bottle, isValid, hex) => {
+    const validString = isValid ? cli.color("green", "valid") : cli.color("red", "INVALID");
+    cli.display(cli.paint(pad(), "[", validString, " hash: ", hex, "]"));
+  });
+
+  reader.on("encrypt", (bottle) => {
+    cli.display(cli.paint(pad(), "[encrypted for: ", bottle.header.recipients.join(", "), "]"));
+  });
+
+  return reader.scanStream(helpers.readStream(cli, filename));
+}
+
+function dumpArchiveFiles(filenames, loudness) {
+  return Promise.map(filenames, ((filename) => dumpArchiveFile(filename, loudness)), { concurrency: 1 });
+}
+
+function dumpArchiveFile(filename, { cli, keybaser, isVerbose, isQuiet }) {
+  // count total bytes packed away
+  const state = { totalBytesIn: 0, totalBytes: 0, totalFiles: 0, prefix: [] };
+
+  const countingInStream = toolkit.countingStream();
+  countingInStream.on("count", (n) => {
+    state.totalBytesIn = n;
+  });
+  helpers.readStream(cli, filename).pipe(countingInStream);
+
+  const reader = new lib4q.ArchiveReader();
+
+  reader.decryptKey = (keymap) => {
+    if (Object.keys(keymap).length == 0) {
+      if (password == null) throw new Error("No password provided.");
+      return Promise.promisify(crypto.pbkdf2)(password, helpers.SALT, 10000, 48);
+    }
+    return keybaser.check().then(() => {
+      const self = `keybase:${keybaser.identity}`;
+      const allowed = Object.keys(keymap).join(", ");
+      if (!keymap[self]) throw new Error(`No encryption key for ${self} (only: ${allowed})`);
+      return keybaser.decrypt(keymap[self]);
+    });
+  };
+
+  reader.on("start-bottle", (bottle) => {
+    switch (bottle.typeName()) {
+      case "file":
+      case "folder":
+        const nicePrefix = state.prefix.join("/") + (state.prefix.length > 0 ? "/" : "");
+        cli.displayNotice(helpers.summaryLineForFile(cli, bottle.header, nicePrefix, isVerbose));
+        state.prefix.push(bottle.header.filename);
+        if (!bottle.header.folder) {
+          state.totalFiles += 1;
+          state.totalBytes += bottle.header.size;
+        }
+        break;
+    }
+  });
+
+  reader.on("end-bottle", (bottle) => {
+    switch (bottle.typeName()) {
+      case "file":
+      case "folder":
+        state.prefix.pop();
+        break;
+    }
+  });
+
+  reader.on("hash", (bottle, isValid, hex) => {
+    // FIXME display something if this is per-file
+    if (!isValid) throw new Error("Invalid hash; archive is probably corrupt.");
+    if (state.prefix.length == 0) state.validHash = bottle.header.hashName;
+  });
+
+  reader.on("compress", (bottle) => {
+    // FIXME display something if this is per-file
+    if (state.prefix.length == 0) state.compression = bottle.header.compressionName;
+  });
+
+  reader.on("encrypt", (bottle) => {
+    // FIXME display something if this is per-file
+    if (state.prefix.length == 0) {
+      state.encryption = bottle.header.encryptionName;
+      if (bottle.header.recipients.length > 0) state.encryptedFor = bottle.header.recipients.join(" & ");
+    }
+  });
+
+  return reader.scanStream(countingInStream).then(() => {
+    const annotations = [];
+    const importante = [];
+    if (state.encryption != null) {
+      importante.push(state.encryption + (state.encryptedFor != null ? ` for ${state.encryptedFor}` : ""));
+    }
+    if (state.compression != null) annotations.push(state.compression);
+    if (state.validHash != null) annotations.push(state.validHash);
+    const compressionStatus =
+      state.compression != null ?
+      cli.paint(" -> ", cli.color(COLORS.file_size, cli.toMagnitude(state.totalBytesOut, 1024) + "B")) :
+      "";
+    const sizes = cli.color(COLORS.file_size, `(${state.totalFiles} files, ${cli.toMagnitude(state.totalBytesIn)}B)`);
+    let extras = importante.length > 0 ? cli.color(COLORS.importante, ` [${importante.join("; ")}]`) : "";
+    extras += annotations.length > 0 ? cli.color(COLORS.annotations, ` [${annotations.join(", ")}]`) : "";
+
+    cli.display(`${filename} ${sizes}${extras}`);
+  }).catch((error) => {
+    cli.displayError(error.stack);
+  })
+}
+
+exports.main = main;
---
+++ src/4q/qpack.js
@@ -0,0 +1,255 @@
+const clicolor = require("clicolor");
+const crypto = require("crypto");
+const fs = require("fs");
+const helpers = require("./helpers");
+const lib4q = require("lib4q");
+const minimist = require("minimist");
+const path = require("path");
+const Promise = require("bluebird");
+const sprintf = require("sprintf");
+const stream = require("stream");
+const strftime = require("strftime");
+const toolkit = require("stream-toolkit");
+const util = require("util");
+
+require("source-map-support").install();
+
+const PACKAGE = require("../../package.json");
+const COLORS = helpers.COLORS;
+const NOW = Date.now();
+
+const USAGE = `
+usage: qpack [options] <filename(s)...>
+    create a 4Q archive from a set of files (or folders)
+
+options:
+    --help
+    -o <filename>
+        archive filename to write
+    -v
+        verbose: display files as they're written
+    -q
+        quiet: display nothing unless there's an error
+    -Z, --no-compress
+        do not compress the contents
+    -S, --snappy
+        use snappy compression instead of LZMA2
+    -H, --no-hash
+        do not compute a check hash (let go and use the force)
+    -e <user>, --encrypt <user>
+        encrypt archive for a keybase user (may be used multiple times to
+        send to multiple recipients)
+    --no-color
+        turn off cool console colors
+`;
+
+function main() {
+  const cli = clicolor.cli();
+  const keybaser = new (require("./keybaser").Keybaser)(cli);
+
+  const argv = minimist(process.argv.slice(2), {
+    alias: { "Z": "no-compress", "H": "no-hash", "S": "snappy", "e": "encrypt" },
+    boolean: [ "help", "version", "v", "q", "color", "compress", "snappy", "debug" ],
+    default: { color: true, compress: true, hash: true }
+  });
+  // minimist isn't great at decoding -Z:
+  if (argv["no-compress"]) argv.compress = false;
+  if (argv["no-hash"]) argv.hash = false;
+  if (argv.help || argv._.length == 0) {
+    console.log(USAGE);
+    process.exit(0);
+  }
+  if (argv.version) {
+    console.log(`qpack ${PACKAGE.version}`);
+    process.exit(0);
+  }
+  if (argv._.length == 0) {
+    console.log("Required: filename(s) to archive");
+    process.exit(1);
+  }
+  if (!argv.color) cli.useColor(false);
+  cli.quiet(argv.q);
+  if (argv.o == null) {
+    if (argv._.length > 1) {
+      argv.o = "archive.4q";
+    } else {
+      let archiveFolder = argv._[0];
+      if (archiveFolder[archiveFolder.length - 1] == "/") {
+        archiveFolder = archiveFolder.slice(0, archiveFolder.length - 1);
+      }
+      argv.o = archiveFolder + ".4q";
+    }
+  }
+
+  // quick sanity check: do all these files exist?
+  let okay = true;
+  argv._.forEach((filename) => {
+    if (!fs.existsSync(filename)) {
+      console.log(`Can't find file: ${filename}`);
+      okay = false;
+    }
+  });
+  if (!okay) process.exit(1);
+
+  function die(message, error) {
+    cli.displayError(`${message}: ${helpers.messageForError(error)}`);
+    if (argv.debug) console.log(error.stack);
+    process.exit(1);
+  };
+
+  conditionally(
+    argv.encrypt,
+    () => {
+      if (!Array.isArray(argv.encrypt)) argv.encrypt = [ argv.encrypt ];
+      return keybaser.check().catch((error) => {
+        die("Keybase error", error);
+      });
+    }
+  ).then(() => {
+    let fd = null;
+    try {
+      fd = fs.openSync(argv.o, "w");
+    } catch (error) {
+      die(`Unable to write ${argv.o}`, error);
+    }
+    const outStream = fs.createWriteStream("", { fd });
+    toolkit.promisify(outStream);
+
+    const state = {
+      fileCount: 0,
+      totalBytesOut: 0,
+      totalBytesIn: 0,
+      currentFileBytes: 0,
+      currentFileTotalBytes: 0,
+      currentFilename: null
+    };
+    const countingOutStream = toolkit.countingStream();
+    countingOutStream.on("count", (n) => {
+      state.totalBytesOut = n;
+      cli.status(statusMessage(cli, state));
+    });
+    countingOutStream.pipe(outStream);
+    let targetStream = countingOutStream;
+
+    if (argv.hash) {
+      const hashBottle = new lib4q.HashBottleWriter(lib4q.HASH_SHA512);
+      hashBottle.pipe(targetStream);
+      targetStream = hashBottle;
+    }
+
+    return conditionally(
+      argv.encrypt || argv.password,
+      () => {
+        return (argv.encrypt ?
+          setupEncryptBottle(keybaser, argv.encrypt) :
+          setupPasswordBottle(argv.password)
+        ).then((encryptedBottle) => {
+          encryptedBottle.pipe(targetStream);
+          encryptedBottle.on("error", (error) => die("Encryption error", error));
+          targetStream = encryptedBottle;
+        })
+      }
+    ).then(() => {
+      if (argv.compress) {
+        const compressionType = argv.snappy ? lib4q.COMPRESSION_SNAPPY : lib4q.COMPRESSION_LZMA2;
+        const compressedBottle = new lib4q.CompressedBottleWriter(compressionType);
+        compressedBottle.pipe(targetStream);
+        targetStream = compressedBottle;
+      }
+
+      const writer = new lib4q.ArchiveWriter();
+      writer.on("filename", (filename, header) => {
+        if (argv.v) printFinishedFile(cli, state);
+        state.currentFileBytes = 0;
+        state.currentFileTotalBytes = header.size;
+        state.currentFilename = filename;
+        state.isFolder = header.folder;
+        if (!header.folder) {
+          state.fileCount += 1;
+          state.totalBytesIn += header.size;
+        }
+        cli.status(statusMessage(cli, state));
+      });
+      writer.on("status", (filename, byteCount) => {
+        state.currentFileBytes = byteCount;
+        cli.status(statusMessage(cli, state));
+      });
+      writer.on("error", (error) => die("Error", error));
+
+      let nextPromise = null;
+      if (argv._.length > 1) {
+        // multiple files: just make a fake folder
+        const folderName = path.basename(argv.o, ".4q");
+        nextPromise = writer.archiveFiles(folderName, argv._);
+      } else {
+        nextPromise = writer.archiveFile(argv._[0]);
+      }
+      return nextPromise.then((bottle) => {
+        bottle.pipe(targetStream);
+        return outStream.finishPromise();
+      }).then(() => {
+        if (argv.v) printFinishedFile(cli, state);
+        const compressionStatus = argv.compress ?
+          cli.paint(" -> ", cli.color(COLORS.file_size, cli.toMagnitude(state.totalBytesOut, 1024) + "B")) : "";
+        const inStatus = cli.color(
+          COLORS.file_size,
+          `(${state.fileCount} files, ${cli.toMagnitude(state.totalBytesIn)}B)`
+        );
+        cli.displayNotice(`${argv.o} ${inStatus}${compressionStatus}`);
+      });
+    });
+  }).catch((error) => {
+    cli.displayError(`Unable to write ${argv.o}: ${helpers.messageForError(error)}`);
+    if (argv.debug) console.log(error.stack);
+    process.exit(1);
+  });
+}
+
+function setupEncryptBottle(keybaser, recipients, targetStream) {
+  const encrypter = (recipient, buffer) => {
+    const [ scheme, name ] = recipient.split(":");
+    if (scheme != "keybase") throw new Error(`Expected keybase scheme, got ${scheme}`);
+    return keybaser.encrypt(buffer, name);
+  };
+
+  // keybase is the only encryption recipient type, so far.
+  recipients = recipients.map((name) => `keybase:${name}`);
+  return lib4q.writeEncryptedBottle(lib4q.ENCRYPTION_AES_256, recipients, encrypter);
+}
+
+// this is really bad. don't use it.
+function setupPasswordBottle(password, targetStream) {
+  return Promise.promisify(crypto.pbkdf2)(password, helpers.SALT, 10000, 48).then((keyBuffer) => {
+    return lib4q.writeEncryptedBottle(lib4q.ENCRYPTION_AES_256, [], keyBuffer);
+  });
+}
+
+function statusMessage(cli, state) {
+  if (!state.currentFilename) return;
+  const count = cli.color(COLORS.status_count, sprintf("%6s", state.fileCount));
+  const totalProgress = cli.color(
+    COLORS.status_total_progress,
+    sprintf("%5s -> %5s", cli.toMagnitude(state.totalBytesIn, 1024), cli.toMagnitude(state.totalBytesOut, 1024))
+  );
+  const fileProgress = state.currentFileBytes > 0 && state.currentFileTotalBytes ?
+    cli.color(
+      COLORS.status_file_progress,
+      `(${Math.floor(100 * state.currentFileBytes / state.currentFileTotalBytes)}%)`
+    ) :
+    "";
+  return cli.paint(count, ": (", totalProgress, ")  ", state.currentFilename, " ", fileProgress);
+}
+
+function printFinishedFile(cli, state) {
+  if (!state.currentFilename) return;
+  const bytes = state.isFolder ? "     " :
+    cli.color(COLORS.file_size, sprintf("%5s", cli.toMagnitude(state.currentFileTotalBytes)));
+  cli.displayNotice(cli.paint("  ", bytes, "  ", state.currentFilename));
+}
+
+function conditionally(predicate, f) {
+  return predicate ? f() : Promise.resolve();
+}
+
+
+exports.main = main;
